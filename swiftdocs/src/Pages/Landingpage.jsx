88c41a1b85feaa61551156e2a987d9852507c62f
Labyrinth759 <valenzuelajoshua759@gmail.com>
2026-03-11 10:40:12 +0800

    first code for the front end

---
+++ swiftdocs/src/Pages/Landingpage.jsx
@@ -0,0 +1,10 @@
+import Nav from "../Components/navigation";
+
+function Landingpage() {
+    return (
+        <>
+        <Nav />
+        </>
+    )
+}
+export default Landingpage;
--- swiftdocs/src/main.jsx
+++ swiftdocs/src/main.jsx
@@ -1,7 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import Landingpage from "./Pages/Landingpage";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>{/* Routes */}</StrictMode>,
+  <StrictMode>
+    <Landingpage />
+  </StrictMode>,
 );

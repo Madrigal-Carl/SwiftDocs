489ca69609698c042cd39afff06abd108a2e2495
Madrigal-Carl <carlsalido.madrigal@gmail.com>
2026-03-11 09:36:47 +0800

    relationship between student, request, receipt, document models done

--- backend/src/database/models/receipt.js
+++ backend/src/database/models/receipt.js
@@ -8,7 +8,12 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Receipt.belongsTo(models.Request, {
+        foreignKey: "request_id",
+        as: "request",
+        onDelete: "CASCADE",
+        onUpdate: "CASCADE",
+      });
     }
   }
   Receipt.init(

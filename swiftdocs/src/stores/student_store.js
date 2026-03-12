62e41c4bc8d837f5834ff72c45a853de0b61eb8e
Madrigal-Carl <carlsalido.madrigal@gmail.com>
2026-03-12 10:40:33 +0800

    fix socket backend and frontend (#11)

--- swiftdocs/src/stores/student_store.js
+++ swiftdocs/src/stores/student_store.js
@@ -1,22 +1,15 @@
-import { createContext, useContext, useState } from "react";
-
 const StudentContext = createContext();
 
 export function StudentProvider({ children }) {
   const [allStudents, setAllStudents] = useState([]);
-  const [studentsByStatus, setStudentsByStatus] = useState({});
 
   const updateAllStudents = (students) => setAllStudents(students);
-  const updateStudentsByStatus = (status, students) =>
-    setStudentsByStatus((prev) => ({ ...prev, [status]: students }));
 
   return (
     <StudentContext.Provider
       value={{
         allStudents,
-        studentsByStatus,
         updateAllStudents,
-        updateStudentsByStatus,
       }}
     >
       {children}

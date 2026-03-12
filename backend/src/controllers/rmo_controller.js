26e05f81a77d4c095ecbac15a5c7ec4c8c0060ab
JoseDoms718 <josedominique018@gmail.com>
2026-03-12 16:27:16 +0800

    finished minor adjustment, added socket for the PATCh and Added a front end service for the RMO

--- backend/src/controllers/rmo_controller.js
+++ backend/src/controllers/rmo_controller.js
@@ -1,11 +1,17 @@
-const rmoService = require("../services/rmo_service");
-
 async function UpdateRequestStatus(req, res) {
+  const io = req.app.get("io");
+
   const request = await rmoService.UpdateRequestStatus(
     Number(req.params.id),
     req.body.status,
     req.user
   );
+
+  // 🔹 Emit realtime update
+  io.emit("requestStatusUpdated", {
+    requestId: Number(req.params.id),
+    status: req.body.status,
+  });
 
   res.json(request);
 }

f79210ea2791090c019934f4bf87a8cf72a4ed58
JoseDoms718 <josedominique018@gmail.com>
2026-03-12 09:56:36 +0800

    Fixed Role implementations

--- backend/src/routes/student_routes.js
+++ backend/src/routes/student_routes.js
@@ -1,18 +1,19 @@
+import requireRole from "../middlewares/role";
 const express = require("express");
 const router = express.Router();
 const studentController = require("../controllers/student_controller");
 const { validateCreateStudent } = require("../validators/student_validator");
 
 // Fetch all students with a specific request status
-router.get("/filter-by-status", studentController.GetStudentsByRequestStatus);
+router.get("/filter-by-status", requireRole("rmo", "cashier"), studentController.GetStudentsByRequestStatus);
 
 // Request documents
 router.post("/", validateCreateStudent, studentController.CreateStudentRequest);
 
 // Fetch all students requested documents
-router.get("/", studentController.GetAllStudentsRequests);
+router.get("/", requireRole("rmo", "cashier", "admin"), studentController.GetAllStudentsRequests);
 
 // Fetch a student's requested documents
-router.get("/:id", studentController.GetStudentRequest);
+router.get("/:id", requireRole("rmo", "cashier", "admin"), studentController.GetStudentRequest);
 
 module.exports = router;

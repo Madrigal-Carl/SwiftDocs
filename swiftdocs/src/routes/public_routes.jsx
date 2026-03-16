ad70d233bd4e576ee07219138f3f2186db9cc8f1
Labyrinth759 <valenzuelajoshua759@gmail.com>
2026-03-16 15:33:26 +0800

    docs

--- swiftdocs/src/routes/public_routes.jsx
+++ swiftdocs/src/routes/public_routes.jsx
@@ -3,12 +3,14 @@ import { lazy } from "react";
 
 const Landingpage = lazy(() => import("../layouts/Landingpage"));
 const DataPrivacy = lazy (() => import("../layouts/DataPrivacy"));
+const DocumentSelection = lazy(() => import("../pages/Public/DocumentSelection"));
 
 export default function PublicRoutes() {
   return (
     <Routes>
       <Route path="*" element={<Landingpage />} />
       <Route path="/privacy" element={<DataPrivacy/>}/>
+      <Route path="/request" element={<DocumentSelection/>}/>
     </Routes>
   );
 }

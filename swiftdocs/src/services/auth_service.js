97b08f33807ecae0538557958abe4bb5468464f8
JoseDoms718 <josedominique018@gmail.com>
2026-03-18 11:24:11 +0800

    finished login

--- swiftdocs/src/services/auth_service.js
+++ swiftdocs/src/services/auth_service.js
@@ -1,5 +1,24 @@
 import api from "../api/api";
 
+// register (admin / rmo / cashier only)
+export async function register(data) {
+  const res = await api.post("/auth/register", data);
+  return res.data;
+}
+
+// login
+export async function login(data) {
+  const res = await api.post("/auth/login", data);
+  return res.data;
+}
+
+// logout
+export async function logout() {
+  const res = await api.post("/auth/logout");
+  return res.data;
+}
+
+// get current authenticated user
 export async function getCurrentUser() {
   const res = await api.get("/auth/me");
   return res.data;
